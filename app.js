45970d4c2ed7fe7da7b77a5ca334d3c7eb5d0e42
Veturi Lakshmi Prathyusha <lakshmiprathyushaveturi@gmail.com>
2022-02-16 19:29:04 +0530

    jsx

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const container = document.getElementById("app")
 
+/*----------------jsx---------------*/
+/*const jsx = 
+<ul>
+   <li> I'm a heading </li>
+   <li> I'm a heading</li>
+   <ul>
+      <li> I'm a heading</li>
+      <li> I'm a heading</li>
+   </ul>
+</ul>*/
+
+
 //Creates element in the virtual DOM.
 //We cannot have multiple render() methods hence we create elements like below.
 //Nesting elements.
-const content = React.createElement('div',{},
-   React.createElement('h1',{},'This is a heading 1')
-   React.createElement('p',{}, 'This is a paragraph')
+const content = React.createElement('div',{},[
+   React.createElement('h1',{},"This is a heading 1")
+   React.createElement('p',{}, "This is a paragraph")
+]
 )
 
 ReactDOM.render(content,container)
