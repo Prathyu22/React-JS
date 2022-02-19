f951655266938c3d1c2e96612f81ca759166d29e
Veturi Lakshmi Prathyusha <lakshmiprathyushaveturi@gmail.com>
2022-02-19 09:39:00 +0530

    Day-5_Navbar,Footer,Content

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,48 +1,14 @@
+import Navbar from './components/layouts/Navbar'
+import Footer from './components/layouts/Footer'
+import Content from './components/Content'
 
-
-function App() {
-
-  const image = "World Economic Forum"
-  
-  let styles 
-
-  if(image === 'World Economic Forum') {
-    styles = {
-      width: "500px",
-      border: "10px solid black"
-    }
-  } else {
-    styles = {
-      width: "200px",
-      border: "10px solid green"
-    }
-  }
-  
-  const handler = () => {
-    console.log("I was hovered")
-  }
-
-  const handler2 = () => {
-    console.log("I was clicked")
-  }
+function App() {  
 
   return (
     <div className="App">
-      <nav>
-        <ul>
-          <li>Home</li>
-        </ul>
-      </nav>
-
-      <div className="container">
-        <h3>{image}</h3> 
-        <img style={styles} src="https://www.weforum.org/assets/fallbacks/generic-large-d3281ac72c5557b91cbf5c81cbf14dd1c4ead6b241ab2ad49afb691f1874ca8b.png" alt="" />
-        <button onMouseMove={handler}>HOVER ME</button>
-        <button onClick={handler2}>CLICK ME</button>
-      </div>
-        <footer>
-          <a href="a">Github</a>
-        </footer>
+      <Navbar />
+      <Content />
+      <Footer />
     </div>
   );
 }
---
+++ myapp/src/components/layouts/Navbar.js
@@ -0,0 +1,9 @@
+const Navbar = () => {
+    return (
+        <nav>
+            <a href="a">HOME</a>
+        </nav>
+    );
+}
+
+export default Navbar;
